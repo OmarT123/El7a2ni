d0d3cd747518ebe29680e6010a124c21414a59fd
OmarT123 <omarabdelaty213@gmail.com>
2023-10-28 13:47:16 +0200

    Add/Update/Delete Health Packages

--- src/Models/HealthPackage.js
+++ src/Models/HealthPackage.js
@@ -3,6 +3,11 @@ const Schema = mongoose.Schema;
 
 const healthPackageSchema = new Schema(
   {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     price: {
       type: Number,
       required: true,
