a07550d70ea39ed249156265ff425edf3a1bb594
OmarT123 <94041039+OmarT123@users.noreply.github.com>
2023-11-04 20:29:35 +0100

    a40-a41 select and view details of doctor from search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar'
 import FamilyMembers from './pages/FamilyMembers';
 import Home from './pages/Home'
 import SearchDoctor from './pages/SearchDoctor';
+import Doctor from './pages/Doctor';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path='/FamilyMembers' element={<FamilyMembers/>}/>
             <Route path='/searchDoctor' element={<SearchDoctor/>}/>
+            <Route path="/doctor" element={<Doctor/>}/>
           </Routes>
         </div>
       </BrowserRouter>
