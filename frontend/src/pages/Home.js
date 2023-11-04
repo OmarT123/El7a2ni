b607ca32b383be5223d1411ac91a802eb305c36a
OmarT123 <94041039+OmarT123@users.noreply.github.com>
2023-11-04 16:12:28 +0100

    Family Member page and add new family member

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+    <div className='Home'>
+      <p>This project is still under construction; please use the paths in the URL to skip to the different pages.</p>
+    </div>
+    );
+  }
+  
+  export default Home
