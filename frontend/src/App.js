1479427b67e4256e8260d7d47e7561cf90ed9604
OmarT123 <94041039+OmarT123@users.noreply.github.com>
2023-11-04 13:06:23 +0100

    Added navbar and some basic styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import NavBar from './components/NavBar'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <div className='pages'>
           <Routes>
-            
+
           </Routes>
         </div>
       </BrowserRouter>
