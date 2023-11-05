f97073fc8a6f14aae6d3832c33c727087b7e1702
OmarT123 <94041039+OmarT123@users.noreply.github.com>
2023-11-05 16:36:08 +0100

    added register as patient and apply as doctor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import NavBar from './components/NavBar'
-import FamilyMembers from './pages/FamilyMembers';
+import FamilyMembers from './pages/patient/FamilyMembers';
 import Home from './pages/Home'
-import SearchDoctor from './pages/SearchDoctor';
-import Doctor from './pages/Doctor';
+import SearchDoctor from './pages/patient/SearchDoctor';
+import Doctor from './pages/doctor/Doctor';
+import Prescription from './pages/patient/Prescription';
+import RegisterPatient from './pages/patient/RegisterPatient';
+import RegisterDoctor from './pages/doctor/RegisterDoctor';
+
 function App() {
   return (
     <div className="App">
@@ -16,6 +20,9 @@ function App() {
             <Route path='/FamilyMembers' element={<FamilyMembers/>}/>
             <Route path='/searchDoctor' element={<SearchDoctor/>}/>
             <Route path="/doctor" element={<Doctor/>}/>
+            <Route path="/viewPrescriptions" element={<Prescription/>}/>
+            <Route path="/register" element={<RegisterPatient />} />
+            <Route path="/registerDoctor" element={<RegisterDoctor />} />
           </Routes>
         </div>
       </BrowserRouter>
