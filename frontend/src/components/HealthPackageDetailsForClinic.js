fb129a76bb03ceb239887e0c5451f470a68e0641
Yasmine-elassiouty <127673094+yasmineelassiouty@users.noreply.github.com>
2023-12-07 00:31:24 +0100

    View Health Packages by patient
    patient can see the health packages' options and details

--- frontend/src/components/HealthPackageDetailsForClinic.js
+++ frontend/src/components/HealthPackageDetailsForClinic.js
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom'
-const HealthPackageDetails = ({hpackage}) => {
+const HealthPackageDetailsForClinic = ({hpackage}) => {
     const pageLink = "/HealthPackageInfo?id="+hpackage._id
     return (
         <Link to={pageLink}>
@@ -7,11 +7,10 @@ const HealthPackageDetails = ({hpackage}) => {
                 <h4>{hpackage.name}</h4>
                 <p><b>Price:</b> {hpackage.price} L.E</p>
                 <p><b>Doctor Discount:</b> {hpackage.doctorDiscount} %</p>
-                <p><b>Medicine Discount:</b> {hpackage.medicineDiscount} %</p>
                 <p><b>Family Discount:</b> {hpackage.familyDiscount} %</p>
             </div>
         </Link>
     )
 }
 
-export default HealthPackageDetails
+export default HealthPackageDetailsForClinic
